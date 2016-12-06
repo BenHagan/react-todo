bceb00a51b4afec6bab2e1ee32ed214efc1867a7
Ben Hagan <benhagan@users.noreply.github.com>
2016-12-05 22:45:53 -0600

    Add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -51,14 +51,14 @@ describe('TodoAPI', () => {
     })
   });
 
-  describe('filterTodos', () => {
+  describe.only('filterTodos', () => {
     var todos = [{
       id: 1,
       text: 'Some text',
       completed: true
     },{
       id: 2,
-      text: 'Some text here',
+      text: 'Other text here',
       completed: false
     },{
       id: 3,
@@ -74,6 +74,22 @@ describe('TodoAPI', () => {
     it('should return only uncompleted items if showCompleted is false', () => {
       var filteredTodos = TodoAPI.filterTodos(todos, false, '');
       expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
     });
   });
 });
